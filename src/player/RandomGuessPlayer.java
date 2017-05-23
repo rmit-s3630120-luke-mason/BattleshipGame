97b2341be81e8fd9d6916ffd2003a0f8e0e36acf
rmit-s3448342-Jake-Williams <s3448342@student.rmit.edu.au>
2017-05-23 18:05:20 +1000

    completed random player

--- src/player/RandomGuessPlayer.java
+++ src/player/RandomGuessPlayer.java
@@ -1,6 +1,6 @@
 package player;
 
-import java.util.Scanner;
+import java.util.*;
 import world.World;
 
 /**
@@ -11,41 +11,148 @@ import world.World;
  */
 public class RandomGuessPlayer implements Player{
 
-    @Override
-    public void initialisePlayer(World world) {
-        // To be implemented.
-    } // end of initialisePlayer()
+    private World world;
+
+    private int numRows;
+    private int numCols;
+    private int numShips;
+
+    private List<World.Coordinate> hits; // list of hits recieved from opponent
+
+    private List<Guess> availableGuesses; // list of guesses that haven't already been made
 
     @Override
-    public Answer getAnswer(Guess guess) {
-        // To be implemented.
+    public void initialisePlayer(World world) 
+    {
+        this.world = world;
 
-        // dummy return
-        return null;
-    } // end of getAnswer()
+        this.numRows = world.numRow;
+        this.numCols = world.numColumn;
 
+        this.numShips = world.shipLocations.size();    
+
+        this.hits = new ArrayList<World.Coordinate>();
+
+        this.availableGuesses = new ArrayList<Guess>();
+        
+        // Initialise availableGuesses with every set of coordinates
+        for (int i = 0; i < this.numRows; i++) 
+        {
+            for (int j = 0; j < this.numCols; j++)
+            {
+                Guess possibleGuess = new Guess();
+                possibleGuess.row = i;
+                possibleGuess.column = j;
+                availableGuesses.add(possibleGuess);
+            }
+        }
+    } 
 
     @Override
-    public Guess makeGuess() {
-        // To be implemented.
+    public Answer getAnswer(Guess guess) 
+    {
+        // check if coordinates given in guess match coordinates of any of our ships
+        // If so, record hit in hits List and check if corresponding ship is now dead
 
-        // dummy return
-        return null;
-    } // end of makeGuess()
+        Answer answer = new Answer();
+        
+        int x = guess.row;
+        int y = guess.column;
 
+        List<World.ShipLocation> shipLocations = world.shipLocations;
 
-    @Override
-    public void update(Guess guess, Answer answer) {
-        // To be implemented.
-    } // end of update()
+        for (int i = 0; i < shipLocations.size(); i++)
+        {
+            List<World.Coordinate> coordinates = shipLocations.get(i).coordinates;
 
+            for (int j = 0; j < coordinates.size(); j++)
+            {
+                World.Coordinate current = coordinates.get(j);
 
-    @Override
-    public boolean noRemainingShips() {
-        // To be implemented.
+                if (x == current.row && y == current.column)
+                {
+                    World.Coordinate hit = world.new Coordinate();
+                    hit.row = x;
+                    hit.column = y;
+                    this.hits.add(hit);
 
-        // dummy return
+                    answer.isHit = true;
+
+                    if (dead(coordinates))
+                    {
+                        answer.shipSunk = shipLocations.get(i).ship;
+                        this.numShips--; 
+                    }
+                }
+            }
+        }
+
+        return answer;
+    } 
+
+    public boolean dead(List<World.Coordinate> coordinates)
+    {
+        // compare ship coordinates with list of hits recieved
+        // If every one of the ships coordinates has a hit recorded, return true
+
+        for (int i = 0; i < coordinates.size(); i++)
+        {
+            boolean match = false;
+            
+            World.Coordinate ship = coordinates.get(i);
+
+            for (int j = 0; j < this.hits.size(); j++)
+            {
+                World.Coordinate hit = this.hits.get(j);
+
+                if (hit.row == ship.row && hit.column == ship.column)
+                {
+                    match = true;
+                    break;
+                }
+            }
+
+            if (!match)
+                return false;
+        }
+ 
         return true;
-    } // end of noRemainingShips()
+    }
 
-} // end of class RandomGuessPlayer
+    @Override
+    public Guess makeGuess() 
+    {
+        // Get a random number within size of availableGuesses list
+        // select that coordinate set and remove from available guesses list
+
+        Random random = new Random();
+
+        int choices = this.availableGuesses.size();
+
+        int choice = random.nextInt(choices);
+
+        Guess guess = this.availableGuesses.get(choice);
+
+        this.availableGuesses.remove(choice);
+
+        return guess;
+
+    } 
+
+
+    @Override
+    public void update(Guess guess, Answer answer) 
+    {
+       // NOT NEEDED FOR RANDOM PLAYER 
+    } 
+
+
+    @Override
+    public boolean noRemainingShips() 
+    {
+        if (this.numShips == 0)
+            return true;
+        return false;
+    } 
+} 
+
